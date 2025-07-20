32512c89b70f964eb306ef1436550d34ce4f062e
Sarthak S. Waghmare <sarthakw7@gmail.com>
2025-07-20 08:13:43 +0530

    added keyboard inputs for pause,speed and quit

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"conway-life/game"
 	"conway-life/render"
+	"conway-life/input"
 	"fmt"
 	"time"
 )
@@ -12,11 +13,45 @@ import (
 func main() {
 	grid := game.InitializeGrid()
 
-	for i := 0; i <= 50; i++ {
+	if err := input.InitKeyboard(); err != nil {
+		panic(err)
+	}
+	defer input.CloseKeyboard()
+
+	delay := 200 * time.Millisecond
+	running := true
+	genration := 0
+
+	for {
 		render.ClearScreen()
 		render.PrintGrid(grid)
-		fmt.Println("Generation:", i)
-		time.Sleep(200 * time.Millisecond)
-		grid = game.NextGeneration(grid)
+		fmt.Println("Generation:", genration)
+		fmt.Println("Controls: [p] pause/resume | [+/-] | [q] quit")
+
+		if running {
+			grid = game.NextGeneration(grid)
+			genration++
+		}
+
+		time.Sleep(delay)
+
+		if char, _ := input.ReadKey(); char != 0 {
+			switch char {
+			case 'q':
+				fmt.Println("Exiting...")
+				return
+			
+			case 'p':
+				running = !running
+			
+			case '+':
+				if delay > 50*time.Millisecond {
+					delay -= 50 * time.Millisecond
+				}
+
+			case '-':
+				delay += 50 * time.Millisecond
+			}
+		}
 	}
 }
